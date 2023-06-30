9d0e85a28bbc26fa3ab56d3bb550552c984a52f8
yiweiding <yiweiding@yding402.gatech.edu>
2023-06-30 10:36:35 -0400

    remove robustness in evaluation

--- eval_model.py
+++ eval_model.py
@@ -6,16 +6,12 @@ from data import LatentSpaceDataModule
 from models import TeacherStudentModel
 
 
-def eval_model(config, ckpt_path, manifest_path, robust_mode=None, robust_rate=None):
+def eval_model(config, ckpt_path, manifest_path):
     with open(config) as f:
         config = json.load(f)
 
     data_cfg = config["data"]
     data_cfg["test_manifest_path"] = manifest_path
-    if robust_mode is not None:
-        print("Using robust mode {} with rate {}".format(robust_mode, robust_rate))
-        data_cfg["robust_mode"] = robust_mode
-        data_cfg["robust_rate"] = robust_rate
     datamodule = LatentSpaceDataModule(**data_cfg)
 
     model = TeacherStudentModel.load_from_checkpoint(ckpt_path)
